d9e895766588f183ae0e72d0a28be71a5844a78c
Fojtík Jáchym <fojtik.jachym.2017@skola.ssps.cz>
2019-12-02 16:15:13 +0100

    Přidejte soubory projektu.

---
+++ AutonomniRizeni/AutonomniAuto.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AutonomniRizeni
+{
+    public enum TrasaStav {Prsi, Mrholi, Mlha, BenatkyNadJizerou, Slunecno}
+    public enum TrasaDruh {Tunel, Most, Common}
+    class AutonomniAuto
+    {
+        public double Rychlost { get; set; }
+        public double DelkaTrasy { get; set; }
+        public bool Sviti { get; set; } = false;
+        public TrasaStav trasaStav;
+        public TrasaDruh trasaDruh;
+        public AutonomniAuto(double Rychlost, double DelkaTrasy, TrasaStav trasaStav, TrasaDruh trasaDruh)
+        {
+            this.Rychlost = Rychlost;
+            this.DelkaTrasy = DelkaTrasy;
+            this.trasaDruh = trasaDruh;
+            this.trasaStav = trasaStav;
+        }
+    }
+}
---
+++ AutonomniRizeni/RidiciCentrum.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AutonomniRizeni
+{
+    class RidiciCentrum : AutonomniAuto
+    {
+        public event DruhTrasy Trasa;
+        public delegate void DruhTrasy(AutonomniAuto a);
+
+        public void ZjistiTrasu()
+        {
+            if (trasaDruh == TrasaDruh.Tunel)
+            {
+                Rychlost = Rychlost + (Rychlost/2);
+                Sviti = true;
+            }
+            else if (trasaDruh == TrasaDruh.Most)
+            {
+                Rychlost = Rychlost - (Rychlost / 2);
+                Sviti = false;
+            }
+            else
+            {
+                Rychlost = Rychlost;
+                Sviti = false;
+            }
+        }
+        public RidiciCentrum(double Rychlost, double DelkaTrasy, TrasaStav trasaStav, TrasaDruh trasaDruh) : base (Rychlost,DelkaTrasy, trasaStav, trasaDruh){}
+    }
+}
